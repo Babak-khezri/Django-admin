a30ec0a00a321b10c8c01813190a672f2a2e5f16
babak_khezri <babak.khezri7@gmail.com>
2021-04-29 14:20:27 +0430

    add footer and new message

--- instagram/templatetags/extra_filters.py
+++ instagram/templatetags/extra_filters.py
@@ -3,9 +3,24 @@ from django.shortcuts import render, get_object_or_404
 from account.models import User
 from django.db.models import Q
 from chat.models import Message
+from account.models import User
+from chat.models import Message
 register = template.Library()
 
 
 @register.simple_tag
 def reminder(value):
     return value % 8
+
+
+@register.simple_tag
+def new_message(username):
+    user = User.objects.get(username=username)
+    try:
+        messages = Message.objects.filter(getter=user)
+        new_msg = [message for message in messages if not message.seen]
+        if len(new_msg) == 0:
+            return ''
+        return len(new_msg) 
+    except:
+        return ''
--- story/admin.py
+++ story/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from .models import Story, Highlight
 # Register your models here.
 
+def make_active_story(modeladmin, request, queryset):
+    queryset.update(status='a')
+    queryset.update(date="")
+    modeladmin.message_user(request, 'مقالات منتشر شدند')
+
 
 class StoryAdmin(admin.ModelAdmin):
     list_display = ('user', 'image_tag', 'status', 'hits_count')
     list_filter = (['user', 'status'])
     ordering = ('-date',)
+    actions = [make_active_story]
 
 
 class HighlightAdmin(admin.ModelAdmin):
